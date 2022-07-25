a3d3b0f253d23caea9df307ec78b4601cea308f7
sincollmm <56375360+sincollmm@users.noreply.github.com>
2022-07-25 15:25:31 +0700

    design main

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/Login/login');
 var addbookRouter = require('./routes/CRUD/AddSach');
 var editbookRouter = require('./routes/CRUD/EditSach');
+var trangchinhRouter = require('./routes/trangchinh/trangchinh');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/addbook', addbookRouter);
 app.use('/editbook', editbookRouter);
+app.use('/trangchu',trangchinhRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/trangchinh/trangchinh.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../../data/dbConfig');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  config.getConnection((err, connection) => {
+    if(err) throw err
+    console.log('da ket noi mysql');
+    connection.query("SELECT sach.Id,sach.Name,sach.Description,sach.Infomation,category.Name AS category_Name FROM `sach`,`category` WHERE sach.category_Id = category.category_Id", (err, rows)=>{
+        if(!err){
+          res.render('trangchinh', { Sach: rows });
+        }else{
+            console.log(err);
+        }
+    });
+  })
+});
+
+
+module.exports = router;
